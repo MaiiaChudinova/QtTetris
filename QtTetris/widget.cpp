3f6c887d9f82c809c6b892a142c310045e2fb2f8
MaiiaChudinova <maya.chudinova@unicyb.kiev.ua>
2019-05-29 11:27:38 +0300

    Fixed bug with block rotation. Found a similar bug with block move from stable blocks(game area refilled incorrectly).

--- QtTetris/widget.cpp
+++ QtTetris/widget.cpp
@@ -294,6 +294,7 @@ void Widget::BlockRotate(int block[4][4])
     for(int i=0;i<4;i++)
         for(int j=0;j<4;j++)
             temp_block[3-j][i]=block[i][j];
+
     for(int i=0;i<4;i++)
         for(int j=0;j<4;j++)
             block[i][j]=temp_block[i][j];
@@ -351,7 +352,9 @@ void Widget::BlockMove(Direction dir)
 
         for(int i=0;i<4;i++)
             for(int j=0;j<4;j++)
+                if (game_area[block_pos.pos_y+i][block_pos.pos_x+j] != 2)
                 game_area[block_pos.pos_y+i][block_pos.pos_x+j]=cur_block[i][j];
+
         // Пересчитать границу
         GetBorder(cur_block,cur_border);
         break;
